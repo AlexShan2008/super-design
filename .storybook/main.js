5f0f12846d4043699052c1605a4161f54bb6aaae
Alex <shantong@reyun.com>
2020-08-16 23:07:15 +0800

    feat: repo init

---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+		"@storybook/addon-viewport",
+		"@storybook/addon-knobs",
+    { name: "@storybook/addon-docs", options: { configureJSX: true } },
+		"@storybook/addon-a11y",
+		"@storybook/addon-storysource",
+  ],
+  webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\.(ts|tsx)$/,
+			use: [
+				{
+					loader: require.resolve("react-docgen-typescript-loader"),
+				},
+			],
+		});
+		config.resolve.extensions.push(".ts", ".tsx");
+		return config;
+	},
+}
---
+++ dev.md
@@ -0,0 +1,37 @@
+# dev
+
+## install react project
+
+```sh
+npx create-react-app r-ui
+```
+
+## cd your project
+
+```sh
+cd r-ui
+```
+
+## install storybook
+
+```sh
+npx -p @storybook/cli sb init --type react_scripts --use-npm
+```
+
+## install dependencies
+
+```sh
+npm i @storybook/addon-storysource -D
+```
+
+## run storybook
+
+```sh
+npm run storybook
+```
+
+## css in js
+
+```sh
+npm install  styled-components @types/styled-components -D
+```
